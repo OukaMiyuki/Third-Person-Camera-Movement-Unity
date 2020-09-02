48914205d5685f87fac1bad213230dbf6a8a0d31
OukaMiyuki <asukaloid.blog@gmail.com>
2020-09-02 16:26:24 +0700

    Third Commit : added some comment in the scripts

--- Assets/Scripts/ThirdPersonCameraController.cs
+++ Assets/Scripts/ThirdPersonCameraController.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 public class ThirdPersonCameraController : MonoBehaviour {
     
     [SerializeField] float rotationSpeed = 1f;
-    [SerializeField] Transform target, player;
+    [SerializeField] Transform target, player; //assign the object of the target and the player (main camera is the child of target object)
 
-    float mouseX, mouseY;
+    float mouseX, mouseY; // assign variable for mouse input rotation
 
     void Start() {
-        Cursor.visible = false;
-        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false; // to hide the cursor
+        Cursor.lockState = CursorLockMode.Locked; //lock the cursor or mouse
     }
 
     void LateUpdate() {
@@ -19,17 +19,17 @@ public class ThirdPersonCameraController : MonoBehaviour {
     }
 
     void CamControl() {
-        mouseX += Input.GetAxis("Mouse X") * rotationSpeed;
-        mouseY -= Input.GetAxis("Mouse Y") * rotationSpeed;
-        mouseY = Mathf.Clamp(mouseY, -35, 60);
+        mouseX += Input.GetAxis("Mouse X") * rotationSpeed; //initiate the mouse X axis (left and right)
+        mouseY -= Input.GetAxis("Mouse Y") * rotationSpeed; // initiate the mouse Y axis (up and down)
+        mouseY = Mathf.Clamp(mouseY, -35, 60); // clamp the up and down rotation so it'll not rotate beyond up and down
 
-        transform.LookAt(target);
+        transform.LookAt(target); //ritate the target object (parent of main camera)
 
-        if (Input.GetKey("w") || Input.GetKey("s") || Input.GetKey("d") || Input.GetKey("a")) {
-            target.rotation = Quaternion.Euler(mouseY, mouseX, 0);
-            player.rotation = Quaternion.Euler(0, mouseX, 0);
-        } else {
-            target.rotation = Quaternion.Euler(mouseY, mouseX, 0);
+        if (Input.GetKey("w") || Input.GetKey("s") || Input.GetKey("d") || Input.GetKey("a")) { //if player move then rotate both camera and player (so you won't see the face of the character)
+            target.rotation = Quaternion.Euler(mouseY, mouseX, 0); //rotate the camera (x, y, z) x = up and down, y = left and right
+            player.rotation = Quaternion.Euler(0, mouseX, 0); //rotate the player left and right alongside with camera horizontal rotation
+        } else { //if player don't move then only rotate the camera (so you be able to see the face of the player)
+            target.rotation = Quaternion.Euler(mouseY, mouseX, 0); // rotate only the camera (target)
         }
     }
 }
--- Assets/Scripts/ThirdPersonCharacterController.cs
+++ Assets/Scripts/ThirdPersonCharacterController.cs
@@ -12,9 +12,9 @@ public class ThirdPersonCharacterController : MonoBehaviour {
     }
 
     void PlayerMovement (){
-        float horInput = Input.GetAxis("Horizontal");
-        float verInput = Input.GetAxis("Vertical");
-        Vector3 playerMovement = new Vector3(horInput, 0f, verInput) * speed * Time.deltaTime;
-        transform.Translate(playerMovement, Space.Self);
+        float horInput = Input.GetAxis("Horizontal"); // get the horizontal input (A & D)
+        float verInput = Input.GetAxis("Vertical"); // get the vertical input (W and S)
+        Vector3 playerMovement = new Vector3(horInput, 0f, verInput) * speed * Time.deltaTime; //create movement vector
+        transform.Translate(playerMovement, Space.Self); // https://docs.unity3d.com/ScriptReference/Space.Self.html
     }
 }
