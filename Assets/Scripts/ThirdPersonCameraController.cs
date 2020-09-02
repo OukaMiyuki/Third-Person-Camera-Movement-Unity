2634d7f1faac088f71e8ae78f2157a1b77228c37
OukaMiyuki <asukaloid.blog@gmail.com>
2020-09-02 16:16:57 +0700

    Second Commit : Addded Player Object & Scripts

---
+++ Assets/Scripts/ThirdPersonCameraController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCameraController : MonoBehaviour {
+    
+    [SerializeField] float rotationSpeed = 1f;
+    [SerializeField] Transform target, player;
+
+    float mouseX, mouseY;
+
+    void Start() {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void LateUpdate() {
+        CamControl();
+    }
+
+    void CamControl() {
+        mouseX += Input.GetAxis("Mouse X") * rotationSpeed;
+        mouseY -= Input.GetAxis("Mouse Y") * rotationSpeed;
+        mouseY = Mathf.Clamp(mouseY, -35, 60);
+
+        transform.LookAt(target);
+
+        if (Input.GetKey("w") || Input.GetKey("s") || Input.GetKey("d") || Input.GetKey("a")) {
+            target.rotation = Quaternion.Euler(mouseY, mouseX, 0);
+            player.rotation = Quaternion.Euler(0, mouseX, 0);
+        } else {
+            target.rotation = Quaternion.Euler(mouseY, mouseX, 0);
+        }
+    }
+}
---
+++ Assets/Scripts/ThirdPersonCharacterController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCharacterController : MonoBehaviour {
+
+    [SerializeField] float speed;
+
+    // Update is called once per frame
+    void Update() {
+        PlayerMovement();
+    }
+
+    void PlayerMovement (){
+        float horInput = Input.GetAxis("Horizontal");
+        float verInput = Input.GetAxis("Vertical");
+        Vector3 playerMovement = new Vector3(horInput, 0f, verInput) * speed * Time.deltaTime;
+        transform.Translate(playerMovement, Space.Self);
+    }
+}
